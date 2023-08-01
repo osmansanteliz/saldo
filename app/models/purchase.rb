be8e8dc68a08bb2dfa56a713c273b56cbaede688
Osman Santeliz <osanteliz88@gmail.com>
2023-08-01 11:35:23 -0600

    *Ultimos retoques en css terminados y algunas mejoras a la aplicacion.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,6 +2,7 @@ class Purchase < ApplicationRecord
   belongs_to :user
   validates :monto, presence: true
   validates :purchase_date, presence: true
+  validates :numero, presence: true
   validates_presence_of :numero
   attr_accessor :numero
   before_save :set_purchase_date
